3567d5e848b7247495dc0ff171defff08beee128
ruizjose93 <148266397+ruizjose93@users.noreply.github.com>
2023-11-06 11:16:39 -0800

    Add files via upload

---
+++ CSE-111-Project/load-hospital.sql
@@ -0,0 +1,13 @@
+.mode csv
+
+.import data/doctor.csv doctor
+
+.import data/patient.csv patient
+
+.import data/medication.csv medication
+
+.import data/appointment.csv appointment
+
+.import data/room.csv room
+
+.import data/nurse.csv nurse
