f3a6711d2261d16adaf46259d95a8c120451f0a9
AlbertoG4 <Chiefm701@gmail.com>
2023-12-06 18:04:28 -0800

    UI and Backend
    alot of changes. Mainly backend

--- CSE-111-Project/Interface.py
+++ CSE-111-Project/Interface.py
@@ -64,69 +64,8 @@ class App(ctk.CTk):
 
         self.doc_nurse_button = ctk.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Doctor/Nurse Managment",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-<<<<<<< Updated upstream
-                                                      image=app.chat_image, anchor="w", command=app.doc_nurse_button_event)
-        app.doc_nurse_button.grid(row=6, column=0, sticky="ew")
-
-        app.appearance_mode_menu = ctk.CTkOptionMenu(app.navigation_frame, values=["Light", "Dark", "System"],
-                                                                command=app.change_appearance_mode_event)
-        app.appearance_mode_menu.grid(row=7, column=0, padx=20, pady=20, sticky="s")
-
-
-        def search_patient_data():
-            # Fetch data from entry fields
-            first_name = app.search_first_name.get()
-            last_name = app.search_last_name.get()
-            dob = app.search_dob.get()
-
-            # Database operation
-            try:
-                # Connect to the SQLite database
-                conn = sqlite3.connect('data.sqlite')  # Replace 'data.sqlite' with your actual database file
-                cursor = conn.cursor()
-
-                # SQL query to search for a patient
-                # This query will search for records that match all provided fields
-                query = ''' SELECT * FROM patient 
-                            WHERE FirstName LIKE ? AND LastName LIKE ? AND DateOfBirth = ? '''
-                cursor.execute(query, (first_name + '%', last_name + '%', dob))
-
-                # Fetching the results
-                search_results = cursor.fetchall()
-                for row in search_results:
-                    print(row)  # Print the result or handle it as needed
-
-            except sqlite3.Error as error:
-                print("Failed to search data in sqlite table", error)
-            
-            finally:
-                # Close the database connection
-                if conn:
-                    conn.close()
-
-        # create home frame
-        app.home_frame = ctk.CTkFrame(app, corner_radius=0, fg_color="transparent")
-        app.home_frame.grid_columnconfigure(0, weight=1)
-
-        app.home_frame_large_image_label = ctk.CTkLabel(app.home_frame, text="", image=app.large_test_image)
-        app.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
-
-        app.search_first_name = ctk.CTkEntry(app.home_frame, placeholder_text="First Name")
-        app.search_first_name.grid(row=1, column=0, padx=20, pady=10)
-
-        app.search_last_name = ctk.CTkEntry(app.home_frame, placeholder_text="Last Name")
-        app.search_last_name.grid(row=2, column=0, padx=20, pady=10)
-
-        app.search_dob = ctk.CTkEntry(app.home_frame, placeholder_text="(YYYY-MM-DD)")
-        app.search_dob.grid(row=3, column=0, padx=20, pady=10)
-
-        app.submit_search_button = ctk.CTkButton(app.home_frame, text="Search", command=search_patient_data)
-        app.submit_search_button.grid(row=4, column=0, padx=20, pady=10)
-
-=======
                                                       image=self.chat_image, anchor="w", command=self.doc_nurse_button_event)
         self.doc_nurse_button.grid(row=6, column=0, sticky="ew")
->>>>>>> Stashed changes
 
         self.appearance_mode_menu = ctk.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"],
                                                                 command=self.change_appearance_mode_event)
@@ -161,13 +100,8 @@ class App(ctk.CTk):
         self.entry_last = ctk.CTkEntry(self.add_patient_frame, placeholder_text="Last Name")
         self.entry_last.grid(row=2, column=0, padx=20, pady=10)
 
-<<<<<<< Updated upstream
-        app.entry_bday = ctk.CTkEntry(app.add_patient_frame, placeholder_text="(YYYY-MM-DD)")
-        app.entry_bday.grid(row=3, column=0, padx=20, pady=10)
-=======
         self.entry_bday = ctk.CTkEntry(self.add_patient_frame, placeholder_text="Date of Birth (YYYY-MM-DD)")
         self.entry_bday.grid(row=3, column=0, padx=20, pady=10)
->>>>>>> Stashed changes
 
         self.entry_address = ctk.CTkEntry(self.add_patient_frame, placeholder_text="Address")
         self.entry_address.grid(row=4, column=0, padx=20, pady=10)
@@ -182,98 +116,9 @@ class App(ctk.CTk):
         self.submit_button.grid(row=7, column=0, padx=20, pady=10)
 
 
-<<<<<<< Updated upstream
-                query = "SELECT * FROM room"
-                cursor.execute(query)
-                rooms = cursor.fetchall()
-
-                for room in rooms:
-                    print(room)  # Replace this with your method of displaying the data
-
-            except sqlite3.Error as error:
-                print("Failed to display all rooms", error)
-            finally:
-                if conn:
-                    conn.close()
-
-        def display_rooms_empty():
-            try:
-                conn = sqlite3.connect('data.sqlite')
-                cursor = conn.cursor()
-
-                # Assuming 'CheckOutDate IS NULL' signifies a room is currently occupied
-                query = '''SELECT * FROM room 
-                        WHERE RoomNumber NOT IN 
-                        (SELECT RoomNumber FROM patient_room WHERE CheckOutDate IS NULL)'''
-                cursor.execute(query)
-                empty_rooms = cursor.fetchall()
-
-                for room in empty_rooms:
-                    print(room)
-
-            except sqlite3.Error as error:
-                print("Failed to display available rooms", error)
-            finally:
-                if conn:
-                    conn.close()
-
-        def display_rooms_occupied():
-            try:
-                conn = sqlite3.connect('data.sqlite')
-                cursor = conn.cursor()
-
-                query = '''SELECT * FROM room 
-                        WHERE RoomNumber IN 
-                        (SELECT RoomNumber FROM patient_room WHERE CheckOutDate IS NULL)'''
-                cursor.execute(query)
-                occupied_rooms = cursor.fetchall()
-
-                for room in occupied_rooms:
-                    print(room)
-
-            except sqlite3.Error as error:
-                print("Failed to display non-available rooms", error)
-            finally:
-                if conn:
-                    conn.close()
-        def display_doctors():
-            try:
-                conn = sqlite3.connect('data.sqlite')
-                cursor = conn.cursor()
-                query = '''SELECT * FROM doctor'''
-                cursor.execute(query)
-                occupied_rooms = cursor.fetchall()
-                for room in occupied_rooms:
-                    print(room)
-            except sqlite3.Error as error:
-                print("Failed to display doctors", error)
-            finally:
-                if conn:
-                    conn.close()
-        
-        def display_nurses():
-            try:
-                conn = sqlite3.connect('data.sqlite')
-                cursor = conn.cursor()
-                query = '''SELECT * FROM nurse'''
-                cursor.execute(query)
-                occupied_rooms = cursor.fetchall()
-                for room in occupied_rooms:
-                    print(room)
-            except sqlite3.Error as error:
-                print("Failed to display nurses", error)
-            finally:
-                if conn:
-                    conn.close()
-
-        # create rooms frame
-        app.room_frame = ctk.CTkFrame(app, corner_radius=0, fg_color="transparent")
-        app.room_frame.grid_columnconfigure(0, weight=1)
-=======
         # Display rooms frame
         self.room_frame = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.room_frame.grid_columnconfigure(0, weight=1)
->>>>>>> Stashed changes
 
             #Display all rooms button
         self.submit_button_all = ctk.CTkButton(self.room_frame, text="Display all Rooms", command=self.display_rooms_all)
@@ -287,122 +132,19 @@ class App(ctk.CTk):
         self.submit_button_non_empty = ctk.CTkButton(self.room_frame, text="Display Non-available rooms", command=self.display_rooms_occupied)
         self.submit_button_non_empty.grid(row=2, column=0, padx=20, pady=10)
 
-<<<<<<< Updated upstream
-
-        #create doc and nurse frame
-        app.doc_nurse_frame = ctk.CTkFrame(app, corner_radius=0, fg_color="transparent")
-        app.doc_nurse_frame.grid_columnconfigure(0, weight=1)
-
-        #Display all doctors
-        app.submit_button_all = ctk.CTkButton(app.doc_nurse_frame, text="Display all doctors", command=display_doctors)
-        app.submit_button_all.grid(row=0, column=0, padx=20, pady=10)
-
-        #Display all nurses
-        app.submit_button_empty = ctk.CTkButton(app.doc_nurse_frame, text="Display all nurses", command=display_nurses)
-        app.submit_button_empty.grid(row=1, column=0, padx=20, pady=10)
-
-        def search_appointment():
-            # Fetch data from entry fields
-            patient_name = app.entry_patient_name.get()
-            patient_dob = app.entry_patient_dob.get()
-
-            try:
-                conn = sqlite3.connect('data.sqlite')
-                cursor = conn.cursor()
-
-                # Assuming you have a 'patient' and an 'appointment' table and they are related by 'PatientID'.
-                # This query joins the 'patient' and 'appointment' tables to find appointments
-                # for a patient with a matching name and date of birth.
-                query = ''' SELECT a.AppointmentID, a.AppointmentTime, a.Notes
-                            FROM appointment a
-                            JOIN patient p ON a.PatientID = p.PatientID
-                            WHERE p.FirstName || ' ' || p.LastName LIKE ? AND p.DateOfBirth = ? '''
-
-                # Format patient name for partial matching and execute query
-                cursor.execute(query, ('%' + patient_name + '%', patient_dob))
-                appointments = cursor.fetchall()
-
-                for appointment in appointments:
-                    print(appointment)  # Replace with your method of displaying the data
-
-            except sqlite3.Error as error:
-                print("Failed to search appointments", error)
-            finally:
-                if conn:
-                    conn.close()
-        
-        #search appointment frame
-        app.search_appointment_frame = ctk.CTkFrame(app, corner_radius=0, fg_color="transparent")
-        app.search_appointment_frame.grid_columnconfigure(0, weight=1)
-=======
         #search appointment frame
         self.search_appointment_frame = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.search_appointment_frame.grid(row=1, column=0, padx=20, pady=10)
->>>>>>> Stashed changes
 
         self.entry_patient_name = ctk.CTkEntry(self.search_appointment_frame, placeholder_text="Patient Name")
         self.entry_patient_name.grid(row=1, column=0, padx=20, pady=10)
 
-<<<<<<< Updated upstream
-        app.entry_patient_dob = ctk.CTkEntry(app.search_appointment_frame, placeholder_text="(YYYY-MM-DD)")
-        app.entry_patient_dob.grid(row=2, column=0, padx=20, pady=10)
-
-        app.submit_button = ctk.CTkButton(app.search_appointment_frame, text="Submit", command=search_appointment)
-        app.submit_button.grid(row=3, column=0, padx=20, pady=10)        
-
-        def add_appointment():
-            # Fetch data from entry fields
-            patient_name = app.entry_patient_name.get()
-            patient_dob = app.entry_patient_dob.get()
-            appointment_date = app.entry_appointment_date.get()
-            appointment_time = app.entry_appointment_time.get()
-
-            # Combine date and time into a single datetime object (if necessary)
-            appointment_datetime = datetime.strptime(appointment_date + ' ' + appointment_time, '%Y-%m-%d %I:%M %p')
-
-            try:
-                conn = sqlite3.connect('data.sqlite')
-                cursor = conn.cursor()
-
-                # Assuming you have a 'patient' table and you need to find the PatientID based on name and DOB
-                cursor.execute("SELECT PatientID FROM patient WHERE FirstName || ' ' || LastName = ? AND DateOfBirth = ?", (patient_name, patient_dob))
-                patient_id_result = cursor.fetchone()
-
-                if patient_id_result:
-                    patient_id = patient_id_result[0]
-                    
-                    # SQL query to insert a new appointment
-                    query = ''' INSERT INTO appointment (PatientID, AppointmentTime)
-                                VALUES (?, ?) '''
-                    cursor.execute(query, (patient_id, appointment_datetime))
-                    conn.commit()
-                    print("Appointment added successfully")
-                else:
-                    print("Patient not found")
-
-            except sqlite3.Error as error:
-                print("Failed to add appointment", error)
-            finally:
-                if conn:
-                    conn.close()
-=======
         self.entry_patient_dob = ctk.CTkEntry(self.search_appointment_frame, placeholder_text="Date of Birth (YYYY-MM-DD)")
         self.entry_patient_dob.grid(row=2, column=0, padx=20, pady=10)
->>>>>>> Stashed changes
 
         self.submit_button = ctk.CTkButton(self.search_appointment_frame, text="Submit", command=self.search_appointment)
         self.submit_button.grid(row=3, column=0, padx=20, pady=10)       
         #create/add appointment frame
-<<<<<<< Updated upstream
-        app.add_appointment_frame = ctk.CTkFrame(app, corner_radius=0, fg_color="transparent")
-        app.add_appointment_frame.grid_columnconfigure(0, weight=1)
-
-        app.entry_patient_name = ctk.CTkEntry(app.add_appointment_frame, placeholder_text="Patient Name")
-        app.entry_patient_name.grid_columnconfigure(0, weight=1)
-
-        app.entry_patient_dob = ctk.CTkEntry(app.add_appointment_frame, placeholder_text="(YYYY-MM-DD)")
-        app.entry_patient_dob.grid(row=2, column=0, padx=20, pady=10)
-=======
         self.add_appointment_frame = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
 
         self.entry_patient_name = ctk.CTkEntry(self.add_appointment_frame, placeholder_text="Patient Name")
@@ -410,7 +152,6 @@ class App(ctk.CTk):
 
         self.entry_patient_dob = ctk.CTkEntry(self.add_appointment_frame, placeholder_text="Date of Birth (YYYY-MM-DD)")
         self.entry_patient_dob.grid(row=2, column=0, padx=20, pady=10)
->>>>>>> Stashed changes
 
         self.entry_appointment_date = ctk.CTkEntry(self.add_appointment_frame, placeholder_text="Appointment Date (YYYY-MM-DD)")
         self.entry_appointment_date.grid(row=3, column=0, padx=20, pady=10)
@@ -672,14 +413,9 @@ class App(ctk.CTk):
     def rooms_button_event(self):
         self.select_frame_by_name("Rooms")
 
-<<<<<<< Updated upstream
-    def doc_nurse_button_event(app):
-        app.select_frame_by_name("Doctor_Nurse")
-=======
     def doc_nurse_button_event(self):
         self.select_frame_by_name("Doctor and Nurse")
 
->>>>>>> Stashed changes
 
     def change_appearance_mode_event(self, new_appearance_mode):
         ctk.set_appearance_mode(new_appearance_mode)
